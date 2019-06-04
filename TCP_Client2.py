6b932da42a9f0cfd3f8b06cb3ad4f5c9209d84bd
shishutong <shishutong49@gmail.com>
2019-06-04 15:11:57 +0800

    SocketServer TCP

---
+++ TCP_Client2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+
+from socket import *
+
+HOST = 'localhost'
+PORT = 21577
+BUFSIZE=1024
+ADDR = (HOST, PORT)
+
+while True:
+    tcpCliSock=socket(AF_INET, SOCK_STREAM)
+    tcpCliSock.connect(ADDR)
+    data=raw_input('>')
+    if not data:
+        break
+    tcpCliSock.send('%s\r\n' % data)
+    data=tcpCliSock.recv(BUFSIZE)
+    if not data:
+        break
+    print data.strip()
+    tcpCliSock.close()
---
+++ TCP_Server2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+
+import SocketServer
+
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 21577
+    # Create the server, binding to localhost on port 21577
+    SocketServer.TCPServer.allow_reuse_address = True
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    print " .... waiting for connection"
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
